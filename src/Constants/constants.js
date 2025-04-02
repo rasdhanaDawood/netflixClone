9cfd28e06fed56fa207968d8bb20a3a6d4d10249
rasdhana <rasdhana@gmail.com>
2025-04-02 21:34:06 +0300

    Removed Constants folder from tracking

--- src/Constants/constants.js
+++
@@ -1,9 +0,0 @@
-const baseUrl = 'https://api.themoviedb.org/3'
-const API_KEY = 'f9383095b9dd9b6c1c5065182d1eb62c'
-const imageUrl = 'https://image.tmdb.org/t/p/original'
-
-export {
-    baseUrl,
-    API_KEY,
-    imageUrl,
-}
